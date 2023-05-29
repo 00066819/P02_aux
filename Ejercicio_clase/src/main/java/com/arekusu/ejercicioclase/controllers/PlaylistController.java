0d59a93019ae904d8b70260d04998bfca6b600a6
Joelbmz24 <00014220@uca.edu.sv>
2023-05-29 13:26:40 -0600

    adding structure project

---
+++ Ejercicio_clase/src/main/java/com/arekusu/ejercicioclase/controllers/PlaylistController.java
@@ -0,0 +1,5 @@
+package com.arekusu.ejercicioclase.controllers;
+
+public class PlaylistController {
+
+}
---
+++ Ejercicio_clase/src/main/java/com/arekusu/ejercicioclase/repositories/PlaylistRepository.java
@@ -0,0 +1,5 @@
+package com.arekusu.ejercicioclase.repositories;
+
+public class PlaylistRepository {
+
+}
